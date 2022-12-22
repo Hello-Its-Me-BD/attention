e4abcf289c318b3894d48359d91a9bad307ed66f
Ivan <silvanovich_iy_19@gmail.com>
2022-12-22 18:51:55 +0300

    First version

---
+++ src/head_pose_estimation.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+
+from face_detector import get_face_detector, find_faces
+from face_landmarks import get_landmark_model, detect_marks
+
+face_model = get_face_detector()
+landmark_model = get_landmark_model()
+cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+size = img.shape
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# 3D model points.
+model_points = np.array([
+    (0.0, 0.0, 0.0),  # Nose tip
+    (0.0, -330.0, -65.0),  # Chin
+    (-225.0, 170.0, -135.0),  # Left eye left corner
+    (225.0, 170.0, -135.0),  # Right eye right corner
+    (-150.0, -150.0, -125.0),  # Left Mouth corner
+    (150.0, -150.0, -125.0)  # Right mouth corner
+])
+
+# Camera internals
+focal_length = size[1]
+center = (size[1] / 2, size[0] / 2)
+camera_matrix = np.array(
+    [[focal_length, 0, center[0]],
+     [0, focal_length, center[1]],
+     [0, 0, 1]], dtype="double"
+)
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    if ret:
+        faces = find_faces(img, face_model)
+
+        for face in faces:
+
+            marks = detect_marks(img, landmark_model, face)
+
+            image_points = np.array([
+                marks[30],  # Nose tip
+                marks[8],  # Chin
+                marks[36],  # Left eye left corner
+                marks[45],  # Right eye right corner
+                marks[48],  # Left Mouth corner
+                marks[54]  # Right mouth corner
+            ], dtype="double")
+
+            dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
+            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix,
+                                                                          dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
+
+            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector,
+                                                             translation_vector, camera_matrix, dist_coeffs)
+
+            for p in image_points:
+                cv2.circle(img, (int(p[0]), int(p[1])), 5, (0, 0, 255), -1)
+
+            p1 = (int(image_points[0][0]), int(image_points[0][1]))
+            p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+            cv2.line(img, p1, p2, (255, 0, 0), 3)
+
+        cv2.imshow('img', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+cv2.destroyAllWindows()
+cap.fl
+cap.release()
